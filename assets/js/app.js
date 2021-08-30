a37894f59bea3b9ce9306c0b3886f5739720ca11
COUILLET Dylan <nanotek.developpeur@gmail.com>
2021-08-30 17:42:16 +0200

    fix negative

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,3 @@
-// const coundown = () => {
-//   // const endDate = new Date("Sep 1, 2021 00:00:00").getTime();
-//   // const now = new Date().getTime();
-
-//   // const diff = endDate - now
-//   let timeMinutes = 4;
-  
-//   let timeSeconds = 59
-
-//   do {
-//     timeSeconds -= 1;
-//   } while (i < 0);
-  
-//   // const seconds = 1000;
-//   // const minutes = seconds * 60;
-//   // const hours = minutes * 60;
-//   // const days = hours * 24;
-
-//   // let timeDays = Math.floor(diff / days)
-//   // let timeHours = Math.floor((diff % days) / hours)
-//   // let timeMinutes = Math.floor((diff % hours) / minutes)
-//   // let timeSeconds = Math.floor((diff % minutes) / seconds)
-
-//   // timeDays = timeDays < 10 ? '0' + timeDays : timeDays;
-//   // timeHours = timeHours < 10 ? '0' + timeHours : timeHours;
-//   timeMinutes = timeMinutes < 10 ? '0' + timeMinutes : timeMinutes;
-//   timeSeconds = timeSeconds < 10 ? '0' + timeSeconds : timeSeconds;
-  
-//   // document.getElementById('days').innerHTML = timeDays;
-//   // document.getElementById('hours').innerHTML = timeHours;
-//   document.getElementById('minutes').innerHTML = timeMinutes;
-//   document.getElementById('seconds').innerHTML = timeSeconds;
-// }
-
-// setInterval (coundown,1000);
-
 const start = 5;
 let time = start * 60
 
@@ -47,6 +11,10 @@ function updateClock() {
 
   seconds = seconds < 10 ? '0' + seconds : seconds;
 
+  if (minutes < 0) {
+    minutes = 0;
+    seconds = 0;
+  }
   document.getElementById('minutes').innerHTML = minutes;
   document.getElementById('seconds').innerHTML = seconds;
   time--;
